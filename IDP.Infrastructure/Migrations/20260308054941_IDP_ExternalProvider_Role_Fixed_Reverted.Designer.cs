0dbb68281e6ed77b58536be64a7a68668212bfed
Naeem Raza <Naeem@hazentech.com>
2026-03-08 10:54:00 +0500

    migration updated

--- IDP.Infrastructure/Migrations/20260308054941_IDP_ExternalProvider_Role_Fixed_Reverted.Designer.cs
+++ IDP.Infrastructure/Migrations/20260308054941_IDP_ExternalProvider_Role_Fixed_Reverted.Designer.cs
@@ -4,6 +4,7 @@ using IDP.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IDP.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260308054941_IDP_ExternalProvider_Role_Fixed_Reverted")]
+    partial class IDP_ExternalProvider_Role_Fixed_Reverted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IDP.Infrastructure/Migrations/20260308054941_IDP_ExternalProvider_Role_Fixed_Reverted.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IDP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IDP_ExternalProvider_Role_Fixed_Reverted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoCreateUsers",
+                table: "ClientExternalProviders");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultRoleId",
+                table: "ClientExternalProviders");
+
+            migrationBuilder.RenameColumn(
+                name: "IsEditable",
+                table: "Roles",
+                newName: "IsSystem");
+
+            migrationBuilder.RenameColumn(
+                name: "IsAssignableToExternalUsers",
+                table: "Roles",
+                newName: "IsAssignableToNewUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoCreateUsers",
+                table: "ClientAuthPolicies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultRoleId",
+                table: "ClientAuthPolicies",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoCreateUsers",
+                table: "ClientAuthPolicies");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultRoleId",
+                table: "ClientAuthPolicies");
+
+            migrationBuilder.RenameColumn(
+                name: "IsSystem",
+                table: "Roles",
+                newName: "IsEditable");
+
+            migrationBuilder.RenameColumn(
+                name: "IsAssignableToNewUsers",
+                table: "Roles",
+                newName: "IsAssignableToExternalUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoCreateUsers",
+                table: "ClientExternalProviders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultRoleId",
+                table: "ClientExternalProviders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
