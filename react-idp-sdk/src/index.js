ea342b09f23c714262f5e17bd36dd69709123b03
Naeem Raza <Naeem@hazentech.com>
2026-04-04 16:44:50 +0500

    react-idp-sdk moved to new repo tokenidp-sdks

--- react-idp-sdk/src/index.js
+++
@@ -1,4 +0,0 @@
-export { IdpAuthProvider, useAuth } from "./AuthProvider";
-export { AuthCallback } from "./AuthCallback";
-export { LoginPage } from "./LoginPage";
-export { defaultAuthConfig } from "./config";
