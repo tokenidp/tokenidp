ce0e5abfc83faf08c4a525fe7dd2b8606988281f
Naeem Raza <Naeem@hazentech.com>
2026-04-17 04:33:38 +0500

    UserRolePermissions view updated

--- src/TokenIDP.Infrastructure/Scripts/vUserRolePermissions.sql
+++ src/TokenIDP.Infrastructure/Scripts/vUserRolePermissions.sql
@@ -2,20 +2,32 @@ CREATE VIEW [dbo].[vUserRolePermissions]
  
 AS 
 
-SELECT distinct c.Id, c.[Sequence],
-c.ParentId,
-c.Permissionkey,
-c.PermissionName,
-rc.IsAllowed,
-c.Icon,
-c.AccessUrl,
-r.RoleName,
-ur.UserId,
-c.ControlType
+SELECT
+    c.Id,
+    c.[Sequence],
+    c.ParentId,
+    c.Permissionkey,
+    c.PermissionName,
+    CAST(MAX(CASE WHEN rc.IsAllowed = 1 THEN 1 ELSE 0 END) AS bit) AS IsAllowed,
+    c.Icon,
+    c.AccessUrl,
+    MIN(r.RoleName) AS RoleName,
+    ur.UserId,
+    c.ControlType
 FROM   dbo.[Permissions] c 
 INNER JOIN dbo.RolePermissions rc ON c.Id = rc.PermissionId 
 INNER JOIN dbo.Roles r ON rc.RoleId = r.Id 
 INNER JOIN dbo.UserRoles ur ON r.Id = ur.RoleId
 Where (r.IsDeleted = 0 OR r.IsDeleted IS NULL) AND r.IsActive = 1
+GROUP BY
+    c.Id,
+    c.[Sequence],
+    c.ParentId,
+    c.Permissionkey,
+    c.PermissionName,
+    c.Icon,
+    c.AccessUrl,
+    ur.UserId,
+    c.ControlType
 
 GO
