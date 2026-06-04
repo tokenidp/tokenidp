6ec3450c20737218024b482af797938d29d103c2
Naeem Raza <naeemraza.info@gmail.com>
2026-06-04 13:07:58 +0500

    Make null outbox consumer router a no-op

--- src/TokenIDP.Infrastructure/Outbox/Abstractions/NullOutboxConsumerRouter.cs
+++ src/TokenIDP.Infrastructure/Outbox/Abstractions/NullOutboxConsumerRouter.cs
@@ -2,12 +2,10 @@ namespace TokenIDP.Infrastructure.Outbox.Abstractions;
 
 internal class NullOutboxConsumerRouter : IOutboxConsumerRouter
 {
-
     public static readonly NullOutboxConsumerRouter Instance = new();
 
     public IReadOnlyList<string> ResolveConsumers(IDomainEvent evt)
     {
-        throw new NotImplementedException();
+        return Array.Empty<string>();
     }
 }
-
